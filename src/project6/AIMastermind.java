b27d2629fb8654271ba06d2168221e1224354b08
oreo22 <oriana22@utexas.edu>
2015-11-20 17:33:33 -0600

    Trying Git in GitBash

--- src/project6/AIMastermind.java
+++ src/project6/AIMastermind.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
  * Created by Oriana_W on 20/11/2015.
  */
 public class AIMastermind {
+    private static java.util.Random rand = new java.util.Random();
     public void startAI(){
         int numberOfPegs=4; //set the number of pegs we want to use in the game
     }
+    public static int getRandomInt(int max) {
+
+        return rand.nextInt(max);
+    }
     public ArrayList<Peg> generateRandomCode(int num){
         ArrayList<Peg> answer=new ArrayList<Peg>(num);
-        
 
+        return answer;
     }
 }
