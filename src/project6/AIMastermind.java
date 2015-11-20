eb68ec8549e3323732778d656a78fed17f2becd9
oreo22 <oriana22@utexas.edu>
2015-11-20 16:54:30 -0600

    First Commit

---
+++ src/project6/AIMastermind.java
@@ -0,0 +1,17 @@
+package project6;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Oriana_W on 20/11/2015.
+ */
+public class AIMastermind {
+    public void startAI(){
+        int numberOfPegs=4; //set the number of pegs we want to use in the game
+    }
+    public ArrayList<Peg> generateRandomCode(int num){
+        ArrayList<Peg> answer=new ArrayList<Peg>(num);
+        
+
+    }
+}
---
+++ src/project6/Peg.java
@@ -0,0 +1,8 @@
+package project6;
+
+/**
+ * Created by Oriana_W on 20/11/2015.
+ */
+public class Peg {
+
+}
