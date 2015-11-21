42ac191073d183b96d4a582e2c4765ba26a504bd
oreo22 <oriana22@utexas.edu>
2015-11-20 18:24:38 -0600

    Started with the random method

--- src/project6/AIMastermind.java
+++ src/project6/AIMastermind.java
@@ -7,16 +7,8 @@ import java.util.ArrayList;
  */
 public class AIMastermind {
     private static java.util.Random rand = new java.util.Random();
-    public void startAI(){
-        int numberOfPegs=4; //set the number of pegs we want to use in the game
-    }
-    public static int getRandomInt(int max) {
-
-        return rand.nextInt(max);
-    }
     public ArrayList<Peg> generateRandomCode(int num){
         ArrayList<Peg> answer=new ArrayList<Peg>(num);
-
         return answer;
     }
 }
--- src/project6/Peg.java
+++ src/project6/Peg.java
@@ -5,4 +5,5 @@ package project6;
  */
 public class Peg {
 
+
 }
