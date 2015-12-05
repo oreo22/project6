1311b798c205ba0fc96b2242544cee3efa8aabd2
oreo22 <oriana22@utexas.edu>
2015-12-05 16:50:53 -0600

    finished the junit tests?

--- src/project6/tests.java
+++ src/project6/tests.java
@@ -2,17 +2,12 @@ package project6;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-import org.junit.Test;
+import org.junit.*;
 
-import project6.Pegs.BluePeg;
-import project6.Pegs.GreenPeg;
-import project6.Pegs.OrangePeg;
-import project6.Pegs.Peg;
-import project6.Pegs.PurplePeg;
-import project6.Pegs.RedPeg;
-import project6.Pegs.YellowPeg;
+import project6.Pegs.*;
 
 public class tests {
 
@@ -47,6 +42,48 @@ public class tests {
 	        AIMastermind.aiGuess.add(new PurplePeg());
 	        AIMastermind.aiGuessBasedOnFeedback(blackPegCount,whitePegCount);
 	        assert(AIMastermind.possibleCombinations.size()==0);
+	}
+	@Test
+	public void possibleCombinationTest() {
+		//To test this method, we changed the number of avaliable colors in PegCreator by commenting them out
+		AIMastermind.initialguess();
+		assert(AIMastermind.possibleCombinations.size()==(PegCreator.availableColors.size()^(Params.pegNumbertoGuess)));
+	}
+	@ Test
+	public void answerGeneratorTest(){
+		for(int i=0; i<Params.pegNumbertoGuess; i++){
+			assert(PegCreator.availableColors.contains(MasterMindConsole.answerKey.get(i)));
+		}
+	}
+	@Test
+	public void inputCheckTest(){
+		//Changed answer to public from private
+			ArrayList<Integer> colorPositions=new ArrayList<>(Params.pegNumbertoGuess);
+			colorPositions.add(0);
+			MasterMindConsole.answer.put("Yellow",colorPositions );
+			ArrayList<Integer> colorPositions2=new ArrayList<>(Params.pegNumbertoGuess);
+			colorPositions.add(1);
+			MasterMindConsole.answer.put("Blue",colorPositions2 );
+			ArrayList<Integer> colorPositions3=new ArrayList<>(Params.pegNumbertoGuess);
+			colorPositions.add(2);
+			MasterMindConsole.answer.put("Green",colorPositions3 );
+			ArrayList<Integer> colorPositions4=new ArrayList<>(Params.pegNumbertoGuess);
+			colorPositions.add(3);
+			MasterMindConsole.answer.put("Red",colorPositions4);
+		;
+		ArrayList<Peg> testUserInput1=new ArrayList<Peg>(){{ //ROPG returns 2 white pegs
+			add(new RedPeg()); add(new OrangePeg()); add(new PurplePeg()); add(new GreenPeg());}};
+		ArrayList<Peg> feedback1=MasterMindConsole.inputCheck(testUserInput1);
+		ArrayList<Peg> correctFeedback1=new ArrayList<Peg>(){{add(new WhitePeg()); add(new WhitePeg()); }};
+		for(int i=0; i<feedback1.size(); i++){
+			if(feedback1.contains(correctFeedback1.get(i))){
+				feedback1.remove(i);
+				correctFeedback1.remove(i);
+			}
+
+		}
+		assert(feedback1.size()==0 && correctFeedback1.size()==0);
+
 	}
 
 }
